e18c09665b735e90cbe322bf631996d3a7689b21
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2014-11-10 16:28:59 +0100

    include a placeholder test

---
+++ FPJDTests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FPJDTests
+{
+    public class PlaceholderTests
+    {
+        [Fact]
+        public void PlaceholderTest()
+        {
+            Assert.Equal(3, 3);
+        }
+    }
+}
